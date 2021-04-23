7c30f87721918d4e8a96b9b92b3d7b2d59e72b49
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-04-23 19:45:57 -0400

    employee controller

---
+++ EmployeeWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Route("employees")]
+        public List<string> GetEmployees() {
+            EmployeeDbContext dbContext = new EmployeeDbContext();
+            var employees = dbContext.Employees.ToList().Select(e => new { e.FirstName }.ToString()).ToList();
+
+            return employees;
+        }
+
+    }
+}
