e8d51a9fd68e824c36c317ce6e0f049074f0236a
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-04-23 14:45:16 -0400

    entities, dbcontext, database.mdf, connection string

---
+++ DataLayer/EmployeeDbContext.cs
@@ -0,0 +1,45 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DataLayer
+{
+    public class EmployeeDbContext : DbContext
+    {
+        private string _connectionString = null;
+        public EmployeeDbContext()
+        {
+            //To read configuration string from appsettings.json, add the NuGet package "Microsoft.Extensions.Configuration.Json"
+            //This constructor runs when you set this as startup project and run PM>add-migration ...name and PM>update-database
+            //appsettings.json of this project will have the value "Do not copy" for "Copy to Output Directory"
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetConnectionString("EmployeeDb");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Add reference to Microsoft.EntityFrameworkCore.SqlServer from NuGet packages to use "UseSqlServer" method on DbContextOptionsBuilder 
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(new Employee()
+            {
+                ID = 1,
+                FirstName = "John",
+                MiddleName = "",
+                LastName = "Smith",
+                DateOfBirth = DateTime.Now,
+                Position = "Sales Rpresentative"
+            });
+        }
+
+    }
+}
+
+
---
+++ DataLayer/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Position { get; set; }
+    }
+}
