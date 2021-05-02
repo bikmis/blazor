48ba222391f6ca13eb3f832cc48065320b4c49f2
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-01 20:31:44 -0400

    comment added

--- DataLayer/EmployeeDbContext.cs
+++ DataLayer/EmployeeDbContext.cs
@@ -11,9 +11,13 @@ namespace DataLayer
         private string _connectionString = null;
         public EmployeeDbContext()
         {
-            //To read configuration string from appsettings.json, add the NuGet package "Microsoft.Extensions.Configuration.Json"
-            //This constructor runs when you set this as startup project and run PM>add-migration ...name and PM>update-database
-            //appsettings.json of this project will have the value "Do not copy" for "Copy to Output Directory"
+            //To read configuration string from appsettings.json, add the NuGet package "Microsoft.Extensions.Configuration.Json".
+            //This constructor runs when you set this as Startup project and run migration commands (PM>add-migration ...migration..name, PM>update-database)
+            //as we are using the appsettings.json from this project.
+            //The appsettings.json file is used with DataLayer project to run migration commands independently of other projects. This appsettings.json is not used in production
+            //and it is only used during development and is replaced by another appsettings.json from web api project (or mvc or blazor etc). That's why
+            //appsettings.json of this project will have the value "Do not copy" for "Copy to Output Directory" property. To access this property, right-click on appsettings.json
+            //and the appsettings.json file with a connection string from web api (or mvc or blazor etc) will have the property "Copy to Output Directory" set to "Copy if newer".
             IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
             _connectionString = configuration.GetConnectionString("EmployeeDb");
         }
