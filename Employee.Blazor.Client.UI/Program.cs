4dc7018595f63d1727aafdeb8af234f2e3c123df
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-04 19:21:08 -0400

    client ui and razor components

--- Employee.Blazor.Client.UI/Program.cs
+++ Employee.Blazor.Client.UI/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Razor.Components.Library.Services.EmployeeService;
+using Razor.Components.Library.Services.UserService;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Employee.Blazor.Client.UI
@@ -17,6 +15,9 @@ namespace Employee.Blazor.Client.UI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            //AddHttpClient() method will be availble after you install Microsoft.Extensions.Http
+            builder.Services.AddHttpClient<IEmployeeService, EmployeeService>(client => client.BaseAddress = new Uri("https://localhost:44327/"));
+            builder.Services.AddHttpClient<IUserService, UserService>(client => client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/"));
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
