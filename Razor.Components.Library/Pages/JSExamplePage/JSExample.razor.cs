9eca31b76a071df05e88da461db06b72e1544034
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-07 14:53:17 -0400

    IJSruntime to run javascript function

---
+++ Razor.Components.Library/Pages/JSExamplePage/JSExample.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razor.Components.Library.Pages.JSExamplePage
+{
+    public partial class JSExample
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        public async Task ShowAlert() {
+            await JSRuntime.InvokeVoidAsync("showAlert"); //showAlert is the name of a function in js/interop.js file
+        }
+
+    }
+}
---
+++ Razor.Components.Library/wwwroot/scripts/example.js
@@ -0,0 +1,3 @@
+﻿function showAlert() {
+    alert("This is an alert");
+}
