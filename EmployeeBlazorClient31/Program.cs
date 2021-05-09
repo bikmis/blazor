ca8f5402ac0842238124ff3d723287d0151c77d8
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-08 20:54:02 -0400

    dependency injection

--- EmployeeBlazorClient31/Program.cs
+++ EmployeeBlazorClient31/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using RazorClassLibrary31.Services.EmployeeService;
+using RazorClassLibrary31.Services.UserService;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeBlazorClient31
@@ -17,6 +15,9 @@ namespace EmployeeBlazorClient31
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            //AddHttpClient() method will be availble after you install Microsoft.Extensions.Http
+            builder.Services.AddHttpClient<IEmployeeService, EmployeeService>(client => client.BaseAddress = new Uri("https://localhost:44327/"));
+            builder.Services.AddHttpClient<IUserService, UserService>(client => client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/"));
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
