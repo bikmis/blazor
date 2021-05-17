51118dfc48929ef8f3da8fdee811385dce7e15d6
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-17 14:39:08 -0400

    localStorage and sessionStorage

--- RazorClassLibrary31/wwwroot/scripts/example.js
+++ RazorClassLibrary31/wwwroot/scripts/example.js
@@ -14,3 +14,35 @@ function focusOnInputQuestion(element) {
 function closeEmployeeEditModal() {
     $("#employeeEditModal").modal("hide");
 }
+
+function setToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getFromLocalStorage(key) {
+    localStorage.getItem(key);
+}
+
+function removeFromLocalStorage(key) {
+    localStorage.removeItem(key);
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+}
+
+function setToSessionStorage(key, value) {
+    sessionStorage.setItem(key, value);
+}
+
+function getFromSessionStorage(key) {
+    sessionStorage.getItem(key);
+}
+
+function removeFromSessionStorage(key) {
+    sessionStorage.removeItem(key);
+}
+
+function clearSessionStorage() {
+    sessionStorage.clear();
+}
