74492051dcd62feffd2d018068f3805426cafc5f
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-27 16:21:12 -0400

    Access token and refresh token removed from user object

--- RazorClassLibrary31/Models/User.cs
+++ RazorClassLibrary31/Models/User.cs
@@ -11,7 +11,5 @@ namespace RazorClassLibrary31.Models
         [Required]
         public string Email { get; set; }
         public bool IsLoggedIn { get; set; }
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
     }
 }
