e5a5724a5a452d1ad5a4611de1f7763a9c8c1fed
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-27 16:22:23 -0400

    token service replaced user service in http service

--- RazorClassLibrary31/Services/HttpService/HttpService.cs
+++ RazorClassLibrary31/Services/HttpService/HttpService.cs
@@ -1,4 +1,5 @@
 ﻿using RazorClassLibrary31.Services.SerializerService;
+using RazorClassLibrary31.Services.TokenService;
 using RazorClassLibrary31.Services.UserService;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -8,21 +9,21 @@ namespace RazorClassLibrary31.Services.HttpService
 {
     public class HttpService : IHttpService
     {
-        private IUserService userService;
+        private ITokenService tokenService;
         private ISerializerService serializerService;
 
-        public HttpService(IUserService _userService, ISerializerService _serializerService)
+        public HttpService(ITokenService _tokenService, ISerializerService _serializerService)
         {
-            userService = _userService;
+            tokenService = _tokenService;
             serializerService = _serializerService;
         }
 
         public async Task<HttpResponseMessage> SendAsync(HttpClient httpClient, HttpMethod method, string url, object data)
         {
             var request = new HttpRequestMessage(method, url);
-            if (userService.User.AccessToken != null)
+            if (tokenService.AccessToken != null)
             {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", userService.User.AccessToken);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokenService.AccessToken);
             }
             request.Content = serializerService.SerializeToString(data); //enable cors (AllowAnyOrigin & AllowAnyHeader) in web api project to accept any request URL & Content-Type "application/json"
             var response = await httpClient.SendAsync(request);
