9b85a6d7df34b76876639bec9c59f285a3886721
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-27 16:26:13 -0400

    token service gets hydrated from login service

--- RazorClassLibrary31/Services/LoginService/LoginService.cs
+++ RazorClassLibrary31/Services/LoginService/LoginService.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 using RazorClassLibrary31.Services.UserService;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using RazorClassLibrary31.Services.TokenService;
 
 namespace RazorClassLibrary31.Services.LoginService
 {
     //LoginService has been injected for ILoginService using AddHttpClient, which is "scoped" and so any property exposed
-    //by LoginService does not hold value like singleton. That's why IsLoggedIn is exposed from TokenService which is a singleton service.
+    //by LoginService does not hold value like singleton. That's why IsLoggedIn is exposed from UserService which is a singleton service.
     public class LoginService : ILoginService
     {
         private IHttpService httpService;
@@ -19,32 +20,44 @@ namespace RazorClassLibrary31.Services.LoginService
         private ISerializerService serializerService;
         private IJSRuntime jsRuntime { get; set; }       
         private IUserService userService { get; set; }
+        private ITokenService tokenService { get; set; }
 
-        public LoginService(IHttpService _httpService, HttpClient _httpClient, ISerializerService _serializerService, IJSRuntime _jsRuntime, IUserService _userService)
+        public LoginService(IHttpService _httpService, HttpClient _httpClient, ISerializerService _serializerService, IJSRuntime _jsRuntime, IUserService _userService, ITokenService _tokenService)
         {
             httpService = _httpService;
             httpClient = _httpClient;
             serializerService = _serializerService;
             jsRuntime = _jsRuntime;
             userService = _userService;
+            tokenService = _tokenService;
         }
 
         public async Task<bool> LoginUser(Login login)
         {
             var response = await httpService.SendAsync(httpClient, HttpMethod.Post, "api/login", login);
             if (response.IsSuccessStatusCode) {
-                var user = await serializerService.DeserializeToType<User>(response);
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var jsonToken = tokenHandler.ReadJwtToken(user.AccessToken);
-                var email = jsonToken.Claims.ToList().Where(claim => claim.Type == "email").FirstOrDefault().Value;
-                user.IsLoggedIn = true;
-                user.Email = email;
+                var token = await serializerService.DeserializeToType<Token>(response);
+                tokenService.AccessToken = token.AccessToken;
+                var id = int.Parse(readToken(token, "id"));
+                var name = readToken(token, "name");
+                var username = readToken(token, "username");
+                var email = readToken(token, "email");
+                var user = new User() { ID = id, Name = name, Username = username, Email = email, IsLoggedIn = true };
                 userService.User = user;
-                await jsRuntime.InvokeVoidAsync("setToSessionStorage", "refresh_token", user.RefreshToken);
+                await jsRuntime.InvokeVoidAsync("setToSessionStorage", "refresh_token", token.RefreshToken);
                 return true;
             }
             return false;
         }
+
+        private string readToken(Token token, string claimType) {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jsonToken = tokenHandler.ReadJwtToken(token.AccessToken);
+            var claimValue = jsonToken.Claims.ToList().Where(claim => claim.Type == claimType).FirstOrDefault()?.Value;
+            return claimValue;
+        }
+
+
 
     }
 }
