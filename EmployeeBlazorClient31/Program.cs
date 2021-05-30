f10d970f4cdb81ac0419efe8c2ceea98e531d36d
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-05-30 09:17:49 -0400

    regrouped and comments added

--- EmployeeBlazorClient31/Program.cs
+++ EmployeeBlazorClient31/Program.cs
@@ -22,20 +22,26 @@ namespace EmployeeBlazorClient31
             builder.RootComponents.Add<App>("app");
 
             //AddHttpClient() method will be availble after you install Microsoft.Extensions.Http
+            //AddHttpClient is the same as AddScoped
+            //scoped
             builder.Services.AddHttpClient<IEmployeeService, EmployeeService>(client => client.BaseAddress = new Uri("https://localhost:44327/"));
-            builder.Services.AddHttpClient<IAuthenticationService, AuthenticationService>(client => client.BaseAddress = new Uri("https://localhost:44382/")); 
+            builder.Services.AddHttpClient<IAuthenticationService, AuthenticationService>(client => client.BaseAddress = new Uri("https://localhost:44382/"));
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationStateProviderService>();
+            // With the following, everything works except that Authorization fails for unknown reason, and so HttpClient is not injected from here, rather HttpClient is created inside AuthenticationStateProviderService.
+            // builder.Services.AddHttpClient<AuthenticationStateProvider, AuthenticationStateProviderService>(client => client.BaseAddress = new Uri("https://localhost:44382/"));
+
+            //singleton
             builder.Services.AddSingleton<IUserService, UserService>();
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); //This HttpClient is for Weather Forcast (/fetchdata)
+            builder.Services.AddSingleton<IHttpService, HttpService>();
+            builder.Services.AddSingleton<ISerializerService, SerializerService>();
+            builder.Services.AddSingleton<ITokenService, TokenService>();
+
             //Difference among AddSingleton, AddScoped and AddTransient
             builder.Services.AddSingleton<IGuidServiceAddSingleton, GuidService>();
             builder.Services.AddScoped<IGuidServiceAddScoped, GuidService>();
             builder.Services.AddTransient<IGuidServiceAddTransient, GuidService>();
 
-            builder.Services.AddSingleton<IHttpService, HttpService>();
-            builder.Services.AddSingleton<ISerializerService, SerializerService>();
-            builder.Services.AddSingleton<ITokenService, TokenService>();
-
-            builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationStateProviderService>();
 
             // You need to add the following two methods for Authorization to work in the web assembly blazor. In the server sice, you don't need them, they are already built in.
             builder.Services.AddOptions();
