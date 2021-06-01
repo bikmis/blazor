69962e89d7b6823d28a17b6b0a28f42b714b8672
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-01 16:37:23 -0400

    removed unnecessary usings

--- RazorClassLibrary31/Pages/AuthenticationExamplePage/AuthenticationExample.razor.cs
+++ RazorClassLibrary31/Pages/AuthenticationExamplePage/AuthenticationExample.razor.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using RazorClassLibrary31.Services.Authentication_Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RazorClassLibrary31.Pages.AuthenticationExamplePage
