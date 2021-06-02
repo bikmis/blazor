4b1f8ed07c13be8d1af70f098cb52fa58a24af3f
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-01 21:12:28 -0400

    email address on the main layout after login

--- RazorClassLibrary31/Pages/LoginPage/LoginUser.razor.cs
+++ RazorClassLibrary31/Pages/LoginPage/LoginUser.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using RazorClassLibrary31.Models;
 using RazorClassLibrary31.Services.Authentication_Service;
+using RazorClassLibrary31.Services.User_Service;
 using System.Threading.Tasks;
 
 namespace RazorClassLibrary31.Pages.LoginPage
@@ -13,9 +14,16 @@ namespace RazorClassLibrary31.Pages.LoginPage
 
         private Login login { get; set; } = new Login();
 
+        [Parameter]
+        public EventCallback<User> OnLoggedIn { get; set; }
+
+        [Inject]
+        private IUserService userService { get; set; }
+
         private async Task loginUser()
         {
             await ((AuthenticationService)authenticationService).LoginUser(login);
+            await OnLoggedIn.InvokeAsync(userService.User);
         }
     }
 }
---
+++ RazorClassLibrary31/Shared/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using RazorClassLibrary31.Models;
+using RazorClassLibrary31.Services.User_Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary31.Shared
+{
+    public partial class MainLayout
+    {
+        private string emailAddress { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+        private void onLoggedIn(User user)
+        {
+            emailAddress = user.Email;
+        }
+    }
+}
