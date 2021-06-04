64223db9b7a8fe10b57322f56651b33f01c02102
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-04 13:19:57 -0400

    Employee handbook page added

---
+++ RazorClassLibrary31/Pages/EmployeeHandbook/EmployeeHandbook.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+
+namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.EmployeeHandbook
+{
+    partial class EmployeeHandbook
+    {
+
+    }
+}
