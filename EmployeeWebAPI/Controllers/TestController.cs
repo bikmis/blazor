91e66312c11773384d778c3e6a8ae2bfe62cd7f4
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-06 22:20:41 -0400

    test controller added

---
+++ EmployeeWebAPI/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intel.EmployeeManagement.WebAPI.Controllers
+{
+    public class TestController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Ok("Employee resource server is running.");
+        }
+    }
+}
