e5cbb5d0fad6eeb84174e2765a95c80bae2f34e9
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-08 19:22:28 -0400

    Get roles method

--- RazorClassLibrary31/Helper/Utility.cs
+++ RazorClassLibrary31/Helper/Utility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 
@@ -14,6 +15,13 @@ namespace Intel.EmployeeManagement.RazorClassLibrary.Helper
             return claimValue;
         }
 
+        public static IEnumerable<string> GetRoles(string token) {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jsonToken = tokenHandler.ReadJwtToken(token);
+            var claims = jsonToken.Claims.ToList().Where(claim => claim.Type == "role").Select(claim => claim.Value);
+            return claims;
+        }
+
         public static bool IsTokenExpired(string token)
         {
             var expiryInSeconds = long.Parse(ReadToken(token, "exp"));  //exp is in NumericDate - number of seconds from 1970-01-01T00:00:00Z UTC until the specified UTC date/time, ignoring leap seconds.
@@ -21,6 +29,5 @@ namespace Intel.EmployeeManagement.RazorClassLibrary.Helper
             var isExpired = utcTimeNowInSecondsFrom1970 > (expiryInSeconds - 10); //In the UI, token expires 10 seconds earlier, so that we don't have to use a token that is expiring soon and have it rejected by the server as it takes a while to reach the server.
             return isExpired;
         }
-
     }
 }
