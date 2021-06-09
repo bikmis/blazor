54827bffb7322f14954fbef00a304e49858c9b9e
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-09 07:49:14 -0400

    roles coming from database and ui protected based on roles

--- JwtServer31/Controllers/LoginController.cs
+++ JwtServer31/Controllers/LoginController.cs
@@ -10,6 +10,7 @@ using System.Security.Claims;
 using System.Text;
 using Intel.EmployeeManagement.IdentityProvider.Services.Database_Service;
 using Intel.EmployeeManagement.Data.Entities;
+using System.Collections.Generic;
 
 namespace Intel.EmployeeManagement.IdentityProvider.Controllers
 {
@@ -38,9 +39,11 @@ namespace Intel.EmployeeManagement.IdentityProvider.Controllers
                 return Unauthorized();
             }
 
+            var roleNames = databaseService.EmployeeDbContext.Roles.Where(r => r.UserID == user.ID).Select(role => role.RoleName).ToList();
+
             var securityKey = "Your Security Key Goes Here.";
-            var accessToken = createJwt(user, securityKey, issuer: "https://localhost:44382/", audience: "https://localhost:44327/", expiryInMinutes: 1440);        //https://localhost:44327/ is the base address of resource (employee) server
-            var refreshToken = createJwt(user, "Your Refresh Token Security Key Goes Here.", issuer: "https://localhost:44382/", audience: "https://localhost:44382/", expiryInMinutes: 2880);      //https://localhost:44382/ is the base address of token server
+            var accessToken = createJwt(user, roleNames, securityKey, issuer: "https://localhost:44382/", audience: "https://localhost:44327/", expiryInMinutes: 1440);        //https://localhost:44327/ is the base address of resource (employee) server
+            var refreshToken = createJwt(user, roleNames, "Your Refresh Token Security Key Goes Here.", issuer: "https://localhost:44382/", audience: "https://localhost:44382/", expiryInMinutes: 2880);      //https://localhost:44382/ is the base address of token server
             var response = new LoginResponse()
             {
                 AccessToken = accessToken,
@@ -49,19 +52,19 @@ namespace Intel.EmployeeManagement.IdentityProvider.Controllers
             return Ok(response);
         }
 
-        private string createJwt(User user, string securityKey, string issuer, string audience, double expiryInMinutes)
+        private string createJwt(User user, List<string> roleNames, string securityKey, string issuer, string audience, double expiryInMinutes)
         {
             var symmetircSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
             var credentials = new SigningCredentials(symmetircSecurityKey, SecurityAlgorithms.HmacSha256);
 
-            var claims = new[] {
+            var claims = new List<Claim> {
                 new Claim("id", user.ID.ToString()),
                 new Claim("username", user.Username),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim("role", "admin"),
-                new Claim("role", "employee"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
+
+            claims.AddRange(roleNames.Select(roleName => new Claim("role", roleName)));
 
             //https://docs.microsoft.com/en-us/linkedin/shared/authentication/programmatic-refresh-tokens#:~:text=By%20default%2C%20access%20tokens%20are,application%20when%20refresh%20tokens%20expire.
             //Use Utc time. Even if you use local time such as Eastern, the token expiration will convert to Utc.
@@ -80,13 +83,14 @@ namespace Intel.EmployeeManagement.IdentityProvider.Controllers
             var refreshToken = Request.Headers["Authorization"].ToString().Split(" ")[1];
             User user = new User()
             {
-                ID = int.Parse(readToken(refreshToken, "id")),
-                Username = readToken(refreshToken, "username"),
-                Email = readToken(refreshToken, "email")
+                ID = int.Parse(readToken(refreshToken, "id").FirstOrDefault()),
+                Username = readToken(refreshToken, "username").FirstOrDefault(),
+                Email = readToken(refreshToken, "email").FirstOrDefault()
             };
+            var roles = readToken(refreshToken, "role");
+            var issuer = readToken(refreshToken, "iss").FirstOrDefault();
 
-            var issuer = readToken(refreshToken, "iss");
-            var accessToken = createJwt(user, "Your Security Key Goes Here.", issuer, audience: "https://localhost:44327/", expiryInMinutes: 1440);
+            var accessToken = createJwt(user, roles, "Your Security Key Goes Here.", issuer, audience: "https://localhost:44327/", expiryInMinutes: 1440);
 
             //Sending back a new access token and but not the old refresh token which has not expired as yet.
             var response = new AccessTokenResponse()
@@ -96,11 +100,11 @@ namespace Intel.EmployeeManagement.IdentityProvider.Controllers
             return Ok(response);
         }
 
-        private string readToken(string token, string claimType) {
+        private List<string> readToken(string token, string claimType) {
             var handler = new JwtSecurityTokenHandler();
             var readableRefreshtoken = handler.ReadJwtToken(token);
-            var claimValue = readableRefreshtoken.Claims.ToList().Where(claim => claim.Type == claimType).FirstOrDefault().Value;
-            return claimValue;
+            var claimValues = readableRefreshtoken.Claims.ToList().Where(claim => claim.Type == claimType).Select(claim => claim.Value);
+            return claimValues.ToList();
         }
 
     }
