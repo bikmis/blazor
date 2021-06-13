fbfba8fb6264750cc50a0d06c169c68dac6c1ff3
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-13 08:05:16 -0400

    progress spinner added on login page

--- RazorClassLibrary31/Pages/LoginPage/LoginUser.razor.cs
+++ RazorClassLibrary31/Pages/LoginPage/LoginUser.razor.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using System;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LoginPage
@@ -21,12 +22,22 @@ namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LoginPage
 
         private string error { get; set; }
 
+        private bool isLoginInProgress { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            isLoginInProgress = false;
+            return base.OnInitializedAsync();
+        }
+
         private async Task loginUser()
         {
             try
             {
+                isLoginInProgress = true;
                 var response = await ((AuthenticationService)authenticationService).LoginUser(login);
                 if (!response.IsSuccessStatusCode) {
+                    isLoginInProgress = false;
                     if (response.StatusCode == HttpStatusCode.Unauthorized)
                     {
                         error = "Check your username and password.";
@@ -39,6 +50,7 @@ namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LoginPage
             }
             catch (Exception e)
             {
+                isLoginInProgress = false;
                 error = e.Message;
                 await _jsRuntime.InvokeVoidAsync("writeToConsole", e.Message); //This works with both Blazor web assembly and Blazor server.
                 Console.WriteLine(e.Message);  //writes to the browser console in web assembly only, does not work for Blazor server.
