9923c7cae5eb51a751a2c451c20f60eb25d964ab
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-18 17:10:28 -0400

    lifecycle methods page added

---
+++ RazorClassLibrary31/Pages/LifecyclePage/Lifecycle.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LifecyclePage
+{
+    public partial class Lifecycle
+    {
+
+    }
+}
