ea90aa3f3b44955047bebd0775f51d97a19038b2
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-18 19:29:17 -0400

    lifecycle methods

--- RazorClassLibrary31/Pages/LifecyclePage/LifecycleChild.razor.cs
+++ RazorClassLibrary31/Pages/LifecyclePage/LifecycleChild.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -6,39 +7,38 @@ using System.Threading.Tasks;
 
 namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LifecyclePage
 {
-    public partial class Lifecycle
+    public partial class LifecycleChild
     {
-
         private int counter { get; set; }
 
-        private string name { get; set; }
+        [Parameter]
+        public string Greeting { get; set; }
 
-        private string greeting { get; set; }
         protected override Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-                Debug.WriteLine("3. Parent OnAfterRenderAsync ran during firstRender.");
+                Debug.WriteLine("3. Child OnAfterRenderAsync ran during firstRender.");
             }
-            else {
+            else
+            {
                 counter++;
-                Debug.WriteLine($"{counter} Parent OnAfterRenderAsync ran again;");
+                Debug.WriteLine($"{counter} Child OnAfterRenderAsync ran again;");
             }
-            
+
             return base.OnAfterRenderAsync(firstRender);
         }
 
         protected override Task OnParametersSetAsync()
         {
-            Debug.WriteLine("2. Parent OnParametersSetAsync");
+            Debug.WriteLine("2. Child OnParametersSetAsync");
             return base.OnParametersSetAsync();
         }
 
         protected override Task OnInitializedAsync()
         {
-            Debug.WriteLine("1. Parent OnInitializedAsync");
+            Debug.WriteLine("1. Child OnInitializedAsync");
             return base.OnInitializedAsync();
         }
-
     }
 }
