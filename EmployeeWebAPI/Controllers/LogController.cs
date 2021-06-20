28ce772e7409076ea82ea90d63750e28fa3072ba
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-20 16:35:54 -0400

    api for logging

---
+++ EmployeeWebAPI/Controllers/LogController.cs
@@ -0,0 +1,37 @@
+﻿using Intel.EmployeeManagement.Data.Entities;
+using Intel.EmployeeManagement.WebAPI.Models.Logging;
+using Intel.EmployeeManagement.WebAPI.Services.Database_Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intel.EmployeeManagement.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class LogController : Controller
+    {
+        private IDatabaseService databaseService { get; set; }
+
+        public LogController(IDatabaseService _databaseService)
+        {
+            databaseService = _databaseService;
+        }
+
+        [HttpPost]
+        [Route("log")]
+        public IActionResult Log(LogRequest request)
+        {
+            var log = new Log() {
+                LogLevel = request.LogLevel,
+                EventName = request.EventName,
+                CreateDate = request.CreateDate,
+                ExceptionMessage = request.ExceptionMessage,
+                Source = request.Source,
+                StackTrace = request.StackTrace
+            };
+
+            databaseService.EmployeeDbContext.Logs.Add(log);
+            databaseService.EmployeeDbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ EmployeeWebAPI/Models/Logging/LogRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intel.EmployeeManagement.WebAPI.Models.Logging
+{
+    public class LogRequest
+    {
+        public string LogLevel { get; set; }
+        public string EventName { get; set; }
+        public string Source { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
