c8e5691954fde2f980c32d3af194c6f9be327416
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-21 13:29:09 -0400

    log example

---
+++ RazorClassLibrary31/Pages/LogExamplePage/LogExample.razor.cs
@@ -0,0 +1,22 @@
+﻿using Intel.EmployeeManagement.RazorClassLibrary.Services.Employee_Service;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.LogExamplePage
+{
+    public partial class LogExample
+    {
+        //Since [Inject] is commented out, it will be written to the Logs table in the database with a log level of critical.
+
+        // [Inject]
+        private IEmployeeService employeeService { get; set; }         
+
+        private async Task getEmployees() {
+            var empoyees = (await employeeService.GetEmployees()).ToList();
+        }
+
+
+
+    }
+}
