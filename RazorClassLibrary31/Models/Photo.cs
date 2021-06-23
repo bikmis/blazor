8d714b0232fdafc6d15639f10da188596d943347
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-23 13:22:11 -0400

    test example razor page

---
+++ RazorClassLibrary31/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace Intel.EmployeeManagement.RazorClassLibrary.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
---
+++ RazorClassLibrary31/Pages/TestExamplePage/TestExample.razor.cs
@@ -0,0 +1,37 @@
+﻿using Intel.EmployeeManagement.RazorClassLibrary.Models;
+using Intel.EmployeeManagement.RazorClassLibrary.Services.Http_Service;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+
+namespace Intel.EmployeeManagement.RazorClassLibrary.Pages.TestExamplePage
+{
+    public partial class TestExample
+    {
+        HttpClient httpClient = new HttpClient();
+
+        [Inject]
+        private IConfiguration configuration { get; set; }
+
+        public IEnumerable<Photo> photos { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await getPhotos();
+        }
+
+        private async Task getPhotos() {
+            photos = (await httpClient.GetFromJsonAsync<List<Photo>>(configuration["PhotoServiceBaseAddress"] + "photos",
+                new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }));
+        }
+    }
+
+
+}
