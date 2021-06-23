0399c59b958220a3bbe94554f7df1fb3960fdc10
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-23 17:43:38 -0400

    two tests- one with httpclient and mockConfiguration service and the other with mock photo service

--- Intel.EmployeeManagement.BlazorClient.Tests/TestExampleTests.cs
+++ Intel.EmployeeManagement.BlazorClient.Tests/TestExampleTests.cs
@@ -2,7 +2,10 @@
 using Intel.EmployeeManagement.BlazorClient.Tests.Services;
 using Intel.EmployeeManagement.RazorClassLibrary.Pages.TestExamplePage;
 using Intel.EmployeeManagement.RazorClassLibrary.Services.Photo_Service;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Threading;
 using Xunit;
 
 namespace Intel.EmployeeManagement.BlazorClient.Tests
@@ -14,19 +17,32 @@ namespace Intel.EmployeeManagement.BlazorClient.Tests
         [Fact]
         public void NumberOfPhotosIs3()
         {
-            // var configurationService = new ServiceDescriptor(typeof(IConfiguration), new MockConfigurationService());
-            // var httpClientService = new ServiceDescriptor(typeof(HttpClient), new MockHttpClient().HttpClient);
-            // Services.Add(configurationService);
-            // Services.Add(httpClientService);
-
+            var configurationService = new ServiceDescriptor(typeof(IConfiguration), new MockConfigurationService());
+            var httpClientService = new ServiceDescriptor(typeof(HttpClient), new HttpClient());
             var photoService = new ServiceDescriptor(typeof(IPhotoService), new MockPhotoService());
+            Services.Add(configurationService);
+            Services.Add(httpClientService);
             Services.Add(photoService);
 
             var cut = RenderComponent<TestExample>();
-
-           // Thread.Sleep(5000);
-
+            cut.Find("#getPhotos_1").Click();
             cut.Find("#countOfPhoto").MarkupMatches("<div id=countOfPhoto>Count of photos: 3</div>");
+        }
+
+        [Fact]
+        public void NumberOfPhotosIs5000()
+        {
+            var configurationService = new ServiceDescriptor(typeof(IConfiguration), new MockConfigurationService());
+            var httpClientService = new ServiceDescriptor(typeof(HttpClient), new HttpClient());
+            var photoService = new ServiceDescriptor(typeof(IPhotoService), new MockPhotoService());
+            Services.Add(configurationService);
+            Services.Add(httpClientService);
+            Services.Add(photoService);
+
+            var cut = RenderComponent<TestExample>();
+            cut.Find("#getPhotos_2").Click();
+            Thread.Sleep(5000);
+            cut.Find("#countOfPhoto").MarkupMatches("<div id=countOfPhoto>Count of photos: 5000</div>");
         }
     }
 }
