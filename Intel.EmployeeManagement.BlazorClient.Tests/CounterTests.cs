6a5333627c7fc6a7dc9e13152181071e60127af7
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-06-22 22:34:38 -0400

    button click and counter increments

---
+++ Intel.EmployeeManagement.BlazorClient.Tests/CounterTests.cs
@@ -0,0 +1,32 @@
+﻿using Bunit;
+using Intel.EmployeeManagement.RazorClassLibrary.Pages.CounterPage;
+using Intel.EmployeeManagement.RazorClassLibrary.Services.Authentication_Service;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Intel.EmployeeManagement.BlazorClient.Tests
+{
+    public class CounterTests : TestContext
+    {
+        [Fact]
+        public void CounterShouldIncrementWhenClicked()
+        {
+            var authenticationStateProviderService = new ServiceDescriptor(typeof(AuthenticationStateProvider), new MockAuthenticationService());
+            var authenticationService = new ServiceDescriptor(typeof(IAuthenticationService), new MockAuthenticationService());
+            Services.Add(authenticationStateProviderService);
+            Services.Add(authenticationService);
+
+            //Arrange: render the Counter.razor component (cut means component under test)
+            var cut = RenderComponent<Counter>();
+
+            //Act: find and the click the <button> element to increment
+            //the counter the <p> element
+            cut.Find("button").Click();
+
+            //Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+
+    }
+}
---
+++ Intel.EmployeeManagement.BlazorClient.Tests/MockAuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using Intel.EmployeeManagement.RazorClassLibrary.Models;
+using Intel.EmployeeManagement.RazorClassLibrary.Services.AppState_Service;
+using Intel.EmployeeManagement.RazorClassLibrary.Services.Authentication_Service;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel.EmployeeManagement.BlazorClient.Tests
+{
+    public class MockAuthenticationService : AuthenticationStateProvider, IAuthenticationService
+    {
+        public Task<HttpResponseMessage> GetAccessToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GuardRoute()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<HttpResponseMessage> LoginUser(Login login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogoutUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
