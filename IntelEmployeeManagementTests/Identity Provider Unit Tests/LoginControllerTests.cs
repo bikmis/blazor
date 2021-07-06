fec20b77ca95971de4dec5f9ef5c66724cbb33fb
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-07-06 07:42:37 -0400

    login controller tests

--- IntelEmployeeManagementTests/Identity Provider Unit Tests/LoginControllerTests.cs
+++ IntelEmployeeManagementTests/Identity Provider Unit Tests/LoginControllerTests.cs
@@ -1,12 +1,8 @@
-﻿using Intel.EmployeeManagement.Data;
-using Intel.EmployeeManagement.Data.Entities;
-using Intel.EmployeeManagement.IdentityProvider.Controllers;
+﻿using Intel.EmployeeManagement.IdentityProvider.Controllers;
 using Intel.EmployeeManagement.IdentityProvider.Models.Login;
+using Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
@@ -14,11 +10,11 @@ namespace Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests
 {
     public class LoginControllerTests
     {
-        [Fact]
+        [Fact(DisplayName = "Test if you receive tokens when you login")]
         public void TestLogin() {
             //Arrange
-            var employeeDbContext = arrangeDbContext();
-            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            var employeeDbContext = LoginService.ProvideEmployeeDbContextWithInMemoryDatabase();
+            var configuration = LoginService.ProvideConfiguration();
             var loginController = new LoginController(employeeDbContext, configuration);
             var loginRequest = new LoginRequest() { Username = "bikash@gmail.com", Password = "password" };
 
@@ -29,28 +25,6 @@ namespace Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests
             var loginResponse = (LoginResponse)((response as OkObjectResult).Value);
             Assert.NotNull(loginResponse.AccessToken);
             Assert.NotNull(loginResponse.RefreshToken);
-        }
-
-        private EmployeeDbContext arrangeDbContext() {
-            var options = new DbContextOptionsBuilder<EmployeeDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
-            var employeeDbContext = new EmployeeDbContext(options);
-
-            List<User> users = new List<User>() {
-                new User() { ID=1, Username="bikash@gmail.com", Password="password", Email="bikash@gmail.com"},
-                new User() { ID=2, Username="jack@gmail.com", Password="password", Email="jack@gmail.com"}
-            };
-            employeeDbContext.Users.AddRange(users);
-
-            List<Role> roles = new List<Role>()
-            {
-                new Role() { ID = 1, UserID = 1, RoleName = "hr"},
-                new Role() { ID = 2, UserID = 2, RoleName = "data entry"}
-            };
-            employeeDbContext.Roles.AddRange(roles);
-
-            employeeDbContext.SaveChanges();
-            return employeeDbContext;
-        }
+        }       
     }
 }
---
+++ IntelEmployeeManagementTests/Identity Provider Unit Tests/Services/LoginService.cs
@@ -0,0 +1,41 @@
+﻿using Intel.EmployeeManagement.Data;
+using Intel.EmployeeManagement.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests.Services
+{
+    public class LoginService
+    {
+        public static EmployeeDbContext ProvideEmployeeDbContextWithInMemoryDatabase()
+        {
+            var options = new DbContextOptionsBuilder<EmployeeDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var employeeDbContext = new EmployeeDbContext(options);
+
+            List<User> users = new List<User>() {
+                new User() { ID=1, Username="bikash@gmail.com", Password="password", Email="bikash@gmail.com"},
+                new User() { ID=2, Username="jack@gmail.com", Password="password", Email="jack@gmail.com"}
+            };
+            employeeDbContext.Users.AddRange(users);
+
+            List<Role> roles = new List<Role>()
+            {
+                new Role() { ID = 1, UserID = 1, RoleName = "hr"},
+                new Role() { ID = 2, UserID = 2, RoleName = "data entry"}
+            };
+            employeeDbContext.Roles.AddRange(roles);
+
+            employeeDbContext.SaveChanges();
+            return employeeDbContext;
+        }
+
+        public static IConfigurationRoot ProvideConfiguration() {
+            var configurationRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            return configurationRoot;
+        }
+    }
+}
