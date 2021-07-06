f169a244492df89b13435099f31063c0e129e7f1
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-07-06 18:16:25 -0400

    test of access token

--- IntelEmployeeManagementTests/Identity Provider Unit Tests/LoginControllerTests.cs
+++ IntelEmployeeManagementTests/Identity Provider Unit Tests/LoginControllerTests.cs
@@ -1,16 +1,16 @@
 ﻿using Intel.EmployeeManagement.IdentityProvider.Controllers;
+using Intel.EmployeeManagement.IdentityProvider.Models.AccessToken;
 using Intel.EmployeeManagement.IdentityProvider.Models.Login;
 using Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 using Xunit;
 
 namespace Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests
 {
     public class LoginControllerTests
     {
-        [Fact(DisplayName = "Test if you receive tokens when you login")]
+        [Fact(DisplayName = "Test if you receive access and refresh tokens when you login with username and password")]
         public void TestLogin() {
             //Arrange
             var employeeDbContext = LoginService.ProvideEmployeeDbContextWithInMemoryDatabase();
@@ -22,9 +22,31 @@ namespace Intel.EmployeeManagement.Tests.Identity_Provider_Unit_Tests
             var response = loginController.Login(loginRequest);
 
             //Assert
-            var loginResponse = (LoginResponse)((response as OkObjectResult).Value);
+            var loginResponse = (LoginResponse)(response as OkObjectResult).Value;
             Assert.NotNull(loginResponse.AccessToken);
             Assert.NotNull(loginResponse.RefreshToken);
-        }       
+        }
+
+        [Fact(DisplayName = "Test if you receive access token via a request with refresh token")]
+        public void TestAccessToken() {
+            //Arrange
+            var employeeDbContext = LoginService.ProvideEmployeeDbContextWithInMemoryDatabase();
+            var configuration = LoginService.ProvideConfiguration();
+            var loginController = new LoginController(employeeDbContext, configuration);
+            var loginRequest = new LoginRequest() { Username = "bikash@gmail.com", Password = "password" };
+            var loginResponse = loginController.Login(loginRequest);
+            var loginResponseObj = (LoginResponse)((loginResponse as OkObjectResult).Value);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = $"Bearer {loginResponseObj.RefreshToken}";
+            loginController.ControllerContext.HttpContext = httpContext;
+
+            //Act
+            var accessTokenResponse = loginController.AccessToken();
+            var accessTokenResponseObj = (AccessTokenResponse)(accessTokenResponse as OkObjectResult).Value;
+
+            //Assert
+            Assert.NotNull(accessTokenResponseObj.AccessToken);
+        }
     }
 }
