9d366eb81333daaf5dac391f7f28a38af0f83908
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-07-09 08:38:12 -0400

    identity provider tests

--- Intel.EmployeeManagement.IdentityProvider.IntegrationTests/IdentityProviderApiIntegrationTests.cs
+++ Intel.EmployeeManagement.IdentityProvider.IntegrationTests/IdentityProviderApiIntegrationTests.cs
@@ -1,10 +1,15 @@
-using Intel.EmployeeManagement.IdentityProvider;
+using Intel.EmployeeManagement.Data.Entities;
+using Intel.EmployeeManagement.IdentityProvider.Models.AccessToken;
 using Intel.EmployeeManagement.IdentityProvider.Models.Login;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Intel.EmployeeManagement.IntegrationTests
+namespace Intel.EmployeeManagement.IdentityProvider.IntegrationTests
 {
     //https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-5.0
     //At the above site, check "Test app prerequisites" which states that the project should have "<Project Sdk="Microsoft.NET.Sdk.Web">" instead of "<Project Sdk="Microsoft.NET.Sdk">" i.e. specify the Web SDK
@@ -33,6 +38,22 @@ namespace Intel.EmployeeManagement.IntegrationTests
             //Assert
             Assert.NotNull(loginResponse.AccessToken);
             Assert.NotNull(loginResponse.RefreshToken);
+        }
+
+        [Fact(DisplayName = "Get access token with request using refresh token")]
+        public async Task Test_AccessToken() {
+            //Arrange
+            var client = factory.CreateClient();
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            var refreshToken = SharedService.CreateJwt(new User() { ID = 1, Username = "Bikash", Password = "password", Email = "bikash@gmail.com" }, new List<string>() { "admin" }, configuration["RefreshTokenSecurityKey"], configuration["TokenIssuer"], configuration["RefreshTokenAudience"], 100);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", refreshToken);
+
+            //Act
+            var response = await client.PostAsync($"api/accessToken", null);
+            var accessTokenResponse = SharedService.DeserializeToType<AccessTokenResponse>(response);
+
+            //Assert
+            Assert.NotNull(accessTokenResponse.AccessToken);
         }
     }
 }
---
+++ Intel.EmployeeManagement.IdentityProvider.IntegrationTests/SharedService.cs
@@ -0,0 +1,49 @@
+﻿using Intel.EmployeeManagement.Data.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace Intel.EmployeeManagement.IdentityProvider.IntegrationTests
+{
+    public class SharedService
+    {
+        public static string CreateJwt(User user, List<string> roleNames, string securityKey, string issuer, string audience, double expiryInMinutes)
+        {
+            var symmetircSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            var credentials = new SigningCredentials(symmetircSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim> {
+                new Claim("id", user.ID.ToString()),
+                new Claim("username", user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            claims.AddRange(roleNames.Select(roleName => new Claim("role", roleName)));
+
+            //https://docs.microsoft.com/en-us/linkedin/shared/authentication/programmatic-refresh-tokens#:~:text=By%20default%2C%20access%20tokens%20are,application%20when%20refresh%20tokens%20expire.
+            //Use Utc time. Even if you use local time such as Eastern, the token expiration will convert to Utc.
+            var token = new JwtSecurityToken(issuer: issuer, audience: audience, claims: claims, notBefore: null, expires: DateTime.UtcNow.AddMinutes(expiryInMinutes), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static HttpContent Serialize(object data)
+        {
+            var stringContent = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+            return stringContent;
+        }
+
+        public static T DeserializeToType<T>(HttpResponseMessage response)
+        {
+            var deserializedToType = JsonSerializer.Deserialize<T>(response.Content.ReadAsStringAsync().Result, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return deserializedToType;
+        }
+    }
+}
