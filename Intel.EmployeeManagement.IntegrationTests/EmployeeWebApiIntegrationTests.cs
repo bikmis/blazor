30ef83fb9e9dd37798518bd1cd1fed5df8052137
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-07-09 08:44:56 -0400

    removed identity provider reference

--- Intel.EmployeeManagement.IntegrationTests/EmployeeWebApiIntegrationTests.cs
+++ Intel.EmployeeManagement.IntegrationTests/EmployeeWebApiIntegrationTests.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
