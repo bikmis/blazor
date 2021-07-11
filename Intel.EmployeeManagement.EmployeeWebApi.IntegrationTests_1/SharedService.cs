8dd9e49c7908a1dc1e547f3c573fd08f5ac01c21
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-07-11 09:58:53 -0400

    change of project name and folder name

--- Intel.EmployeeManagement.EmployeeWebApi.IntegrationTests_1/SharedService.cs
+++ Intel.EmployeeManagement.EmployeeWebApi.IntegrationTests_1/SharedService.cs
@@ -9,7 +9,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 
-namespace Intel.EmployeeManagement.IntegrationTests
+namespace Intel.EmployeeManagement.EmployeeWebApi.IntegrationTests_1
 {
     public class SharedService
     {
