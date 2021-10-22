b3e6b2569d765658329d772e9298524d8e2679fa
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-10-22 16:10:36 -0400

    only using ms edge driver for e2e

--- Intel.EmployeeManagement.BlazorClientApp.e2eTests/Helper/WebDriver.cs
+++ Intel.EmployeeManagement.BlazorClientApp.e2eTests/Helper/WebDriver.cs
@@ -13,10 +13,10 @@ namespace Intel.EmployeeManagement.BlazorClientApp.e2eTests.Helper
             var driverPath = Path.GetFullPath(@"Helper\Drivers");
             List<IWebDriver> drivers = new List<IWebDriver>();
 
-            var chromeOptions = new ChromeOptions();
+           /* var chromeOptions = new ChromeOptions();
             chromeOptions.BinaryLocation = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
             drivers.Add(new ChromeDriver(driverPath, chromeOptions));
-
+           */
             var edgeOptions = new EdgeOptions();
             edgeOptions.BinaryLocation = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
             drivers.Add(new EdgeDriver(driverPath, edgeOptions));
